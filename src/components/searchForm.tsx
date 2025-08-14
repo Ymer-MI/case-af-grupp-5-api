f396b37451408351e2d39da3d83acbd0f1781594
lillwilma <wilmasxs@hotmail.com>
2025-08-14 13:03:52 +0200

    Code-review update

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { DigiFormInput, DigiButton } from "@digi/arbetsformedlingen-react"
+import { DigiFormInputSearch } from "@digi/arbetsformedlingen-react"
 
 export default function SearchForm() {
   const [title, setTitle] = useState("")
@@ -12,17 +12,14 @@ export default function SearchForm() {
       }}
       style={{ display: "grid", gap: "1rem", maxWidth: "400px" }}
     >
-      <DigiFormInput
-        afLabel="Titel"
+      <DigiFormInputSearch
         value={title}
+        afLabel="Titel"
+        afButtonText="Sök"
         onAfOnInput={(e: CustomEvent<{ value: string }>) =>
           setTitle(e.detail.value)
         }
       />
-
-      <DigiButton afType="submit">
-        Sök
-      </DigiButton>
     </form>
   )
 }
