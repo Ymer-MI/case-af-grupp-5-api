c1b0dedd456c092c4c55bd1c8ec625f629be2a5c
ymer94 <ymer_94@hotmail.com>
2025-08-16 08:40:18 +0200

    Updated styling of the header with added appropriate digi tags.
    Changes to be committed:
    	modified:   src/components/Header.tsx
    	modified:   src/css/Header.css
    	new file:   src/css/color.css
    	modified:   src/main.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
 import '../css/Header.css'
 import { NavLink } from 'react-router'
-import { DigiIconArrowLeft } from '@digi/arbetsformedlingen-react'
+import { DigiHeaderNavigation, DigiHeaderNavigationItem, DigiIconArrowLeft, DigiTypography } from '@digi/arbetsformedlingen-react'
+import { TypographyVariation } from '@digi/arbetsformedlingen'
 
 export const Header = () => {
     return <header>
         <div>
-            { !location.href.endsWith('/') ? <button type='button' onClick={() => history.back()}><DigiIconArrowLeft/>Tillbaka</button> : <></> }
+            { !location.href.endsWith('/') ? <button type='button' onClick={() => history.back()}><DigiIconArrowLeft/> Tillbaka</button> : <></> }
         </div>
-        <h1>Jobbs&ouml;k</h1>
-        <nav>
+        <DigiTypography afVariation={TypographyVariation.LARGE}>
+            <h1>Jobbs&ouml;k</h1>
+        </DigiTypography>
+        <DigiHeaderNavigation afCloseButtonText='St&auml;ng' afCloseButtonAriaLabel='St&auml;ng meny' afNavAriaLabel='Huvudmeny'>
             <ul>
-                <li><NavLink to='/'>Hem</NavLink></li>
+                <li>
+                    <DigiHeaderNavigationItem>
+                        <NavLink to='/'>Hem</NavLink>
+                    </DigiHeaderNavigationItem> 
+                </li>
             </ul>
-        </nav>
+        </DigiHeaderNavigation>
     </header>
 }
