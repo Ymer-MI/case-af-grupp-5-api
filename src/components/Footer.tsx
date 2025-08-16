f8cbef15a9fb9acbb34ce1ab0c186849d4ab9024
ymer94 <ymer_94@hotmail.com>
2025-08-16 14:49:17 +0200

    Updated footer styling with added digi components.
    Changes to be committed:
    	modified:   src/components/Footer.tsx
    	new file:   src/css/Footer.css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,16 @@
+import '../css/Footer.css'
+import { DigiTypography } from '@digi/arbetsformedlingen-react'
+import { TypographyVariation } from '@digi/arbetsformedlingen'
+
 export const Footer = () => {
     return <footer>
-        <div className='grid grid-cols-1 md:grid-cols-3 w-full'>
-            <section>An</section>
-            <section>Item</section>
-            <section>Here</section>
-        </div>
-        <span>{ `\u00a9${new Date().getFullYear()} ` }</span>
+        <DigiTypography afVariation={ TypographyVariation.LARGE }>
+            <section>
+                    <p>An</p>
+                    <p>Item</p>
+                    <p>Here</p>
+            </section>
+            <p>{ `\u00a9${new Date().getFullYear()}` } Grupp 5</p>
+        </DigiTypography>
     </footer>
 }
