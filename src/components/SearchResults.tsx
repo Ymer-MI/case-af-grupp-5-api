b5956b041cdc3a18bd72701e8869f483802045e8
ymer94 <ymer_94@hotmail.com>
2025-08-26 14:40:35 +0200

    Updated: redering the results section with header always.
    Conditional rendering applyed on only the no results text or the results
    section. Also updated the ol to a ul tag (for semantic purposes).
    
    Changes to be committed:
    	modified:   src/components/SearchResultCard.tsx
    	modified:   src/components/SearchResults.tsx
    	modified:   src/css/SearchResultCard.css
    	modified:   src/css/SearchResults.css

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,34 +1,27 @@
+import '../css/SearchResults.css'
 import type { IHit } from '../models/ISearchResult'
-import { TypographyVariation } from '@digi/arbetsformedlingen'
 import { DigiTypography } from '@digi/arbetsformedlingen-react'
 import { SearchResultCard } from './SearchResultCard'
-import "../css/SearchResults.css"
 
 type SearchResultsProps = {
     query: string,
     ads: IHit[]
 }
 
-export const SearchResults = ({ query, ads}: SearchResultsProps) => {
-
-    if (ads.length === 0) {
-        return <DigiTypography>
-            <p>Inga träffar, testa ett annat ord.</p>
-         </DigiTypography>
+export const SearchResults = ({ query, ads}: SearchResultsProps) => <section className='results-container'>
+    <DigiTypography>
+        <h3>Resultat f&ouml;r &ldquo;{ query }&rdquo;</h3>
+    </DigiTypography>
+    { ads.length === 0 ?
+        (<DigiTypography>
+            <p>Inga tr&auml;ffar, testa ett annat ord.</p>
+        </DigiTypography>) :
+        (<section className='results'>
+            <ul>
+                {ads?.map(ad => (
+                    <SearchResultCard key={ ad.id } ad={ ad } />
+                ))}
+            </ul>
+        </section>)
     }
-
-    return <section className='results-container'>
-        <DigiTypography afVariation={ TypographyVariation.SMALL }>
-            <h3>Resultat för &ldquo;{ query }&rdquo;</h3>
-            
-            <section className='results'>
-                <ol>
-                    {ads?.map(ad => (
-                        <SearchResultCard key={ ad.id } ad={ ad } />
-                    ))}
-                </ol>
-            </section>
-            
-        </DigiTypography>
-    </section>
-}
+</section>
