d1a1fbf7a2f99354f67ec90758ed7f88af119e55
ymer94 <ymer_94@hotmail.com>
2025-08-28 16:54:23 +0200

    Updated: CSS to work in firefox too.
    Also som cleanup in chart components code.
    
    Changes to be committed:
    	modified:   src/components/CompetenceChart.tsx
    	modified:   src/css/CompChart.css

--- src/components/CompetenceChart.tsx
+++ src/components/CompetenceChart.tsx
@@ -1,30 +1,21 @@
+import '../css/CompChart.css'
 import type Occupation from '../models/Occupation'
 import { DigiBarChart } from '@digi/arbetsformedlingen-react'
 import { summarizeSkills } from '../helpers/summarizeSkills'
 import type { ChartLineData } from '@digi/arbetsformedlingen'
 import { BarChartVariation } from '@digi/arbetsformedlingen'
-import '../css/CompChart.css'
+
 type CompetenceChartProps = {
     occupations: Occupation[]
 }
 
 export const CompetenceChart = ({ occupations }: CompetenceChartProps) => {
-    if (occupations.length === 0) {
-        return null
-    }
-
-    const skillSummary = summarizeSkills(occupations)
-    
-    const topSkills = skillSummary.skillOccurrences
-        .sort((a, b) => b.occurrences - a.occurrences)
-        
-
-    const chartData: ChartLineData = {
+    const skillSummary = summarizeSkills(occupations).skillOccurrences.sort((a, b) => b.occurrences - a.occurrences), chartData: ChartLineData = {
         data: {
-            xValues: topSkills.map((_, index) => index),
-            xValueNames: topSkills.map(({ skill }) => skill),
+            xValues: skillSummary.map((_, index) => index),
+            xValueNames: skillSummary.map(({ skill }) => skill),
             series: [{
-                yValues: topSkills.map(({ occurrences }) => occurrences),
+                yValues: skillSummary.map(({ occurrences }) => occurrences),
                 title: ''
             }]
         },
@@ -33,13 +24,5 @@ export const CompetenceChart = ({ occupations }: CompetenceChartProps) => {
         title: 'Kompetensöversikt'
     }
 
-    return (
-        <div className="competency-chart-container">
-            <DigiBarChart 
-                afChartData={chartData}
-                afHeadingLevel="h3"
-                afVariation={BarChartVariation.Horizontal}
-            />
-        </div>
-    )
+    return occupations.length && <DigiBarChart afChartData={ chartData } afHeadingLevel='h3' afVariation={ BarChartVariation.Horizontal }/>
 }
