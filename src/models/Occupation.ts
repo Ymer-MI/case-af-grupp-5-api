fc33c55427f935e2c10a0000ea171832604048c8
ymer94 <ymer_94@hotmail.com>
2025-08-28 10:52:26 +0200

    Updated: optimized fetching of skills so it doesn't crash anymore.
    Changes to be committed:
    	modified:   src/models/Occupation.ts
    	modified:   src/services/jobSearchService.ts
    	modified:   src/services/occupationSkillsService.ts

--- src/models/Occupation.ts
+++ src/models/Occupation.ts
@@ -1,25 +1,15 @@
-import OccupationSkillsService from '../services/occupationSkillsService'
 import type { IOccupation } from './ISearchResult'
 import type Skill from './Skill'
 
 export default class Occupation {
-    private skillService = new OccupationSkillsService()
     private id: IOccupation['concept_id']
     private label: IOccupation['label']
-    private skills: Skill[] = []
+    private skills: Skill[]
 
-    constructor({ concept_id, label }: IOccupation) {
+    constructor({ concept_id, label }: IOccupation, skills: Skill[]) {
         this.id = concept_id
         this.label = label
-        this.loadSkills()
-    }
-
-    private async loadSkills() {
-        try {
-            this.skills = await this.skillService.getSkills(this.id)
-        } catch (error) {
-            console.error('Failed to load skills: ', error)
-        }
+        this.skills = skills
     }
 
     public getID = () => this.id
--- src/services/jobSearchService.ts
+++ src/services/jobSearchService.ts
@@ -1,7 +1,9 @@
 import { BaseService } from './baseService'
+import OccupationSkillsService from './occupationSkillsService'
 import { removeDuplicates } from '../helpers/removeDuplicates'
-import type { ISearchResult } from '../models/ISearchResult'
-import Occupation from '../models/Occupation'
+import type { IOccupation, ISearchResult } from '../models/ISearchResult'
+import Occupation from '../models/Occupation';
+import Skill from '../models/Skill'
 
 export class JobSearchService {
     public static DEFAULTS = {
@@ -9,12 +11,29 @@ export class JobSearchService {
         OFFSET: 0
     }
     private api = new BaseService('https://jobsearch.api.jobtechdev.se')
+    private skillService = new OccupationSkillsService()
 
-    public getOccupations = async (query: string, { limit, offset }: { limit?: number, offset?: number }) => removeDuplicates((await this.api.get<ISearchResult>('/search', {
-        params: {
-            q: query,
-            limit: limit ?? JobSearchService.DEFAULTS.LIMIT,
-            offset: offset ?? JobSearchService.DEFAULTS.OFFSET
-        }
-    })).hits ?? []).map(h => new Occupation(h.occupation))
+    public getOccupations = async (query: string, { limit, offset }: { limit?: number, offset?: number }) => {
+        type skillsMapType = [
+            IOccupation['concept_id'],
+            Skill[]
+        ]
+        
+        const uniqueOccupations = removeDuplicates((await this.api.get<ISearchResult>('/search', {
+            params: {
+                q: query,
+                limit: limit ?? JobSearchService.DEFAULTS.LIMIT,
+                offset: offset ?? JobSearchService.DEFAULTS.OFFSET
+            }
+        })).hits ?? []).map(h => h.occupation)
+        
+        const skillsMap = new Map<skillsMapType[0], skillsMapType[1]>(await Promise.all(
+            uniqueOccupations.map(async (o): Promise<skillsMapType> => [
+                o.concept_id,
+                await this.skillService.getSkills(o.concept_id)
+            ])
+        ))
+
+        return uniqueOccupations.map(o => new Occupation(o, skillsMap.get(o.concept_id) ?? []))
+    }
 }
